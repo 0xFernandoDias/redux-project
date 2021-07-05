0cc441aaf76d58f16d24d42ddd22403f10ead1d9
nandOdiaz <fernando_dias99@hotmail.com>
2021-07-05 16:56:04 -0300

    first commit

---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Wrapper } from './AppHeader.styles'
+
+function AppHeader () {
+  return (
+  <Wrapper>
+    <span>
+      e-Supermarket
+    </span>
+  </Wrapper>
+  )
+}
+
+export default AppHeader
